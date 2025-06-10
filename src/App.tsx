c85fdf2cebcb9a47d4919bc6842ecf96801fc6b3
Warren Whipple <w@warrenwhipple.com>
2025-06-10 13:05:11 +0100

    Add shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Button } from "./components/ui/button";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -7,12 +8,9 @@ function App() {
     <div className="flex flex-col items-center justify-center h-screen">
       <h1>Vite + React</h1>
       <div className="card">
-        <button
-          className="bg-blue-500 text-white p-2 rounded-md"
-          onClick={() => setCount((count) => count + 1)}
-        >
+        <Button onClick={() => setCount((count) => count + 1)}>
           count is {count}
-        </button>
+        </Button>
       </div>
     </div>
   );
